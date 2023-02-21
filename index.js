fe35840123b606f5a442ce18fdd99ca462d5bec1
Oliver <olichase91@gmail.com>
2023-02-21 11:26:54 -0800

    Solved Lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(distance){
+  let reply
+  if (distance <=400) {
+    reply = `This one is on me!`
+  }
+  if (distance > 400 && distance <= 2000) {
+    reply = `That will be twenty bucks.`
+  }
+  if (distance > 2000 && distance <= 2500) {
+    reply = `I will gladly take your thirty bucks.`
+  }
+  if (distance > 2500) {
+    reply = `No can do.`
+  }
+  return reply
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return city === `NYC` ? `Ok, sounds good.` : `No go.`
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case `generous`:
+      return 'Thank you so much.'
+    case `not as generous`:
+      return `Thank you.`
+    default: return `Bye.`
+  }
 }
+
+
+
